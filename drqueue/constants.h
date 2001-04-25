414f0866d5ce399a606d85c6f7ad680bff4bf1b8
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-04-25 15:55:24 +0000

    Working on it

--- drqueue/constants.h
+++ drqueue/constants.h
@@ -1,4 +1,4 @@
-/* $Id: constants.h,v 1.1 2001/04/25 10:45:40 jorge Exp $ */
+/* $Id: constants.h,v 1.2 2001/04/25 15:55:23 jorge Exp $ */
 
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
@@ -6,5 +6,6 @@
 #define MAXTASKS 64		/* Maximum number of tasks in the same computer */
 #define MAXNAMELEN 64		/* Maxumum name length for computers and owners */
 #define MAXCMDLEN 256		/* Maximum command length for the task command */
+#define BUFFERLEN 256		/* General buffer len for text */
 
 #endif /* _CONSTANTS_H_ */
