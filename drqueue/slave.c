f90ba456d22e2272c049db859e03f1d89bb61def
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-04-26 16:06:22 +0000

    Working on...
    git-svn-id: https://ssl.drqueue.org/svn/trunk@13 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/slave.c
+++ drqueue/slave.c
@@ -1,6 +1,7 @@
-/* $Id: slave.c,v 1.1 2001/04/26 14:20:55 jorge Exp $ */
+/* $Id: slave.c,v 1.2 2001/04/26 16:06:22 jorge Exp $ */
 
 #include "computer.h"
+#include "logger.h"
 
 int main (int argc,char *argv[])
 {
@@ -8,7 +9,10 @@ int main (int argc,char *argv[])
 
   get_hwinfo (&comp.hwinfo);
   report_hwinfo (&comp.hwinfo);
-  get_computerstatus (&comp.cstatus);
+  get_computerstatus (&comp.status);
+  
+  strcpy (comp.status.task[0].jobname,"Test");
+  log_slave_task (&comp.status.task[0],"Another test");
 
   exit (0);
 }
