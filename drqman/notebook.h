e5e03e21ed55298465797dbd34ac9f88908b68a1
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-07-13 15:23:33 +0000

    Added drqman first files

---
+++ drqman/notebook.h
@@ -0,0 +1,12 @@
+/*
+ * $Header: /root/cvs/drqueue/drqman/notebook.h,v 1.1 2001/07/13 15:23:33 jorge Exp $
+ */
+
+#ifndef _NOTEBOOK_H
+#define _NOTEBOOK_H
+
+#include <gtk/gtk.h>
+
+void CreateNotebook (GtkWidget*,GtkWidget*);
+
+#endif /* _NOTEBOOK_H */
