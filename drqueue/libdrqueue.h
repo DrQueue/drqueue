8b510b35722b3f8d4fbbe325d68751cbb345b0e4
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-13 15:45:57 +0000

    Added libdrqueue.h
    git-svn-id: https://ssl.drqueue.org/svn/trunk@45 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

---
+++ drqueue/libdrqueue.h
@@ -0,0 +1,15 @@
+/* $Id: libdrqueue.h,v 1.1 2001/07/13 15:45:57 jorge Exp $ */
+#ifndef _LIBDRQUEUE_H_
+#define _LIBDRQUEUE_H_
+
+#include "computer.h"
+#include "job.h"
+#include "task.h"
+#include "logger.h"
+#include "communications.h"
+#include "request.h"
+#include "drerrno.h"
+#include "database.h"
+#include "semaphores.h"
+
+#endif /* _libdrqueue_h_ */
