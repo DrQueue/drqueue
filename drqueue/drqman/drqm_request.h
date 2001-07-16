d0dab09e2e9ba268316321dcc7091a4cffce4a56
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-07-16 09:52:50 +0000

    Added to repository

---
+++ drqueue/drqman/drqm_request.h
@@ -0,0 +1,10 @@
+/* $Id: drqm_request.h,v 1.1 2001/07/16 09:52:50 jorge Exp $ */
+
+#ifndef _DRQM_REQUEST_H_
+#define _DRQM_REQUEST_H_
+
+#include "drqm_jobs.h"
+
+void drqm_request_joblist (struct info_drqm_jobs *info);
+
+#endif /* _DRQM_REQUEST_H_ */
