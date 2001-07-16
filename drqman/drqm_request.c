6a4aeeeee3eafdc69927d3a125c35efc741b87df
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-16 09:52:50 +0000

    Added to repository
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@46 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

---
+++ drqman/drqm_request.c
@@ -0,0 +1,9 @@
+/* $Id: drqm_request.c,v 1.1 2001/07/16 09:52:50 jorge Exp $ */
+
+#include <libdrqueue.h>
+#include "drqm_request.h"
+
+void drqm_request_joblist (struct info_drqm_jobs *info)
+{
+
+}
