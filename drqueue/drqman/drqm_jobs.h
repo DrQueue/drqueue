7a001e0143988762292559f7bf27816e0f4defe0
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-17 15:10:12 +0000

    Basic job listing working.
    git-svn-id: https://ssl.drqueue.org/svn/trunk@66 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drqman/drqm_jobs.h
+++ drqueue/drqman/drqm_jobs.h
@@ -1,5 +1,5 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/drqm_jobs.h,v 1.3 2001/07/17 10:21:52 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/drqm_jobs.h,v 1.4 2001/07/17 15:10:12 jorge Exp $
  */
 
 #ifndef _DRQM_JOBS_H
@@ -21,6 +21,7 @@ GtkWidget *CreateJobsList(struct info_drqm_jobs *info);
 GtkWidget *CreateClist (GtkWidget *window);
 GtkWidget *CreateButtonRefresh (struct info_drqm_jobs *info);
 void PressedButtonRefresh (GtkWidget *b, struct info_drqm_jobs *info);
+void drqm_update_joblist (struct info_drqm_jobs *info);
 
 #endif /* _DRQM_JOBS_H */
 
