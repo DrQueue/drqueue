5b76467ea7fd161a9f12b59845f1e3ef52841618
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-17 10:23:12 +0000

    Added drqm_request_joblist and clean_joblist
    git-svn-id: https://ssl.drqueue.org/svn/trunk@56 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drqman/drqm_request.h
+++ drqueue/drqman/drqm_request.h
@@ -1,4 +1,4 @@
-/* $Id: drqm_request.h,v 1.1 2001/07/16 09:52:50 jorge Exp $ */
+/* $Id: drqm_request.h,v 1.2 2001/07/17 10:23:12 jorge Exp $ */
 
 #ifndef _DRQM_REQUEST_H_
 #define _DRQM_REQUEST_H_
@@ -6,5 +6,6 @@
 #include "drqm_jobs.h"
 
 void drqm_request_joblist (struct info_drqm_jobs *info);
+void clean_joblist (struct info_drqm_jobs *info);
 
 #endif /* _DRQM_REQUEST_H_ */
