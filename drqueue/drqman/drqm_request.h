839b244c4dc27228f7c00e1791961888953ec2f5
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-19 09:08:50 +0000

    Added drqm_request_computerlist
    git-svn-id: https://ssl.drqueue.org/svn/trunk@74 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drqman/drqm_request.h
+++ drqueue/drqman/drqm_request.h
@@ -1,11 +1,15 @@
-/* $Id: drqm_request.h,v 1.2 2001/07/17 10:23:12 jorge Exp $ */
+/* $Id: drqm_request.h,v 1.3 2001/07/19 09:08:50 jorge Exp $ */
 
 #ifndef _DRQM_REQUEST_H_
 #define _DRQM_REQUEST_H_
 
 #include "drqm_jobs.h"
+#include "drqm_computers.h"
 
 void drqm_request_joblist (struct info_drqm_jobs *info);
 void clean_joblist (struct info_drqm_jobs *info);
+
+void drqm_request_computerlist (struct info_drqm_computers *info);
+void clean_computerlist (struct info_drqm_computers *info);
 
 #endif /* _DRQM_REQUEST_H_ */
