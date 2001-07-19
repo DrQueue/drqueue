c58543fd58bf3aa8f7348ba45daf851eb853ac52
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-07-19 09:09:15 +0000

    Added call to CreateComputersPage
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@75 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqman/notebook.c
+++ drqman/notebook.c
@@ -1,10 +1,11 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/notebook.c,v 1.1 2001/07/13 15:23:33 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/notebook.c,v 1.2 2001/07/19 09:09:15 jorge Exp $
  */
 
 #include <gtk/gtk.h>
 #include "notebook.h"
 #include "drqm_jobs.h"
+#include "drqm_computers.h"
 
 void CreateNotebook (GtkWidget *window,GtkWidget *vbox)
 {
@@ -14,6 +15,7 @@ void CreateNotebook (GtkWidget *window,GtkWidget *vbox)
   gtk_notebook_set_tab_pos(GTK_NOTEBOOK(notebook),GTK_POS_TOP);
   
   CreateJobsPage(notebook);
+  CreateComputersPage(notebook);
   
   gtk_box_pack_end(GTK_BOX(vbox), notebook, TRUE,TRUE, 0);
 
