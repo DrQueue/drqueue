dcaeeb8f0da1b8d41558149bcfe88e10bc7fe5ee
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-08-02 10:20:24 +0000

    Added dialog for submiting new jobs to the queue.

--- drqueue/drqman/drqm_jobs.c
+++ drqueue/drqman/drqm_jobs.c
@@ -1,8 +1,9 @@
 /*
- * $Id: drqm_jobs.c,v 1.8 2001/07/31 13:06:38 jorge Exp $
+ * $Id: drqm_jobs.c,v 1.9 2001/08/02 10:20:23 jorge Exp $
  */
 
 #include <string.h>
+#include <unistd.h>
 
 #include "drqm_request.h"
 #include "drqm_jobs.h"
@@ -18,7 +19,14 @@ static void PressedButtonRefresh (GtkWidget *b, struct info_drqm_jobs *info);
 static gint PopupMenu(GtkWidget *clist, GdkEvent *event, struct info_drqm_jobs *info);
 static GtkWidget *CreateMenu (struct info_drqm_jobs *info);
 static void JobDetails(GtkWidget *menu_item, struct info_drqm_jobs *info);
+static void NewJob (GtkWidget *menu_item, struct info_drqm_jobs *info);
+static GtkWidget *NewJobDialog (struct info_drqm_jobs *info);
 
+static void dnj_psearch (GtkWidget *button, struct info_dnj *info);
+static void dnj_set_cmd (GtkWidget *button, struct info_dnj *info);
+static void dnj_cpri_changed (GtkWidget *entry, struct info_dnj *info);
+static void dnj_bsubmit_pressed (GtkWidget *button, struct info_dnj *info);
+static int dnj_submit (struct info_dnj *info);
 
 void CreateJobsPage (GtkWidget *notebook)
 {
@@ -169,6 +177,11 @@ static GtkWidget *CreateMenu (struct info_drqm_jobs *info)
   gtk_signal_connect(GTK_OBJECT(menu_item),"activate",GTK_SIGNAL_FUNC(JobDetails),info);
   gtk_widget_show(menu_item);
 
+  menu_item = gtk_menu_item_new_with_label("New Job");
+  gtk_menu_append(GTK_MENU(menu),menu_item);
+  gtk_signal_connect(GTK_OBJECT(menu_item),"activate",GTK_SIGNAL_FUNC(NewJob),info);
+  gtk_widget_show(menu_item);
+
   gtk_signal_connect(GTK_OBJECT((info->clist)),"event",GTK_SIGNAL_FUNC(PopupMenu),info);
 
   gtk_widget_show(menu);
@@ -183,6 +196,268 @@ static void JobDetails(GtkWidget *menu_item, struct info_drqm_jobs *info)
 /*    gtk_grab_add(dialog); */
 }
 
+static void NewJob (GtkWidget *menu_item, struct info_drqm_jobs *info)
+{
+  GtkWidget *dialog;
+  dialog = NewJobDialog(info);
+  gtk_grab_add(dialog);
+}
+
+static GtkWidget *NewJobDialog (struct info_drqm_jobs *info)
+{
+  GtkWidget *window;
+  GtkWidget *frame;
+  GtkWidget *vbox;
+  GtkWidget *hbox,*hbox2;
+  GtkWidget *label;
+  GtkWidget *entry; 
+  GtkWidget *button;
+  GtkWidget *combo;
+  GtkWidget *bbox;
+  GList *items = NULL;
+
+  /* Dialog */
+  window = gtk_window_new (GTK_WINDOW_DIALOG);
+  gtk_window_set_title (GTK_WINDOW(window),"New Job");
+  gtk_signal_connect_object(GTK_OBJECT(window),"destroy",GTK_SIGNAL_FUNC(gtk_widget_destroy),
+			    (GtkObject*)window);
+  gtk_window_set_default_size(GTK_WINDOW(window),600,200);
+  gtk_container_set_border_width (GTK_CONTAINER(window),5);
+  info->dnj.dialog = window;
+
+  /* Frame */
+  frame = gtk_frame_new ("Give job information");
+  gtk_container_add (GTK_CONTAINER(window),frame);
+  gtk_widget_show (frame);
+
+  /* Main vbox */
+  vbox = gtk_vbox_new (FALSE,2);
+  gtk_container_add (GTK_CONTAINER(frame),vbox);
+
+  /* Label */
+  label = gtk_label_new ("Information to be show here");
+  gtk_label_set_pattern (GTK_LABEL(label),"________________________________");
+  gtk_box_pack_start (GTK_BOX(vbox),label,FALSE,FALSE,4);
+  gtk_widget_show (label);
+
+  /* Name of the job */
+  hbox = gtk_hbox_new (TRUE,2);
+  gtk_box_pack_start (GTK_BOX(vbox),hbox,FALSE,FALSE,2);
+  gtk_widget_show (hbox);
+  label = gtk_label_new ("Name:");
+  gtk_label_set_justify (GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+/*    gtk_misc_set_padding (GTK_MISC(label),20,2); */
+  gtk_box_pack_start (GTK_BOX(hbox),label,FALSE,FALSE,2);
+  gtk_widget_show(label);
+  entry = gtk_entry_new_with_max_length (MAXNAMELEN-1);
+  info->dnj.ename = entry;
+  gtk_box_pack_start (GTK_BOX(hbox),entry,TRUE,TRUE,2);
+  gtk_widget_show(entry);
+
+  /* Cmd of the job */
+  hbox = gtk_hbox_new (TRUE,2);
+  gtk_box_pack_start (GTK_BOX(vbox),hbox,FALSE,FALSE,2);
+  gtk_widget_show (hbox);
+  label = gtk_label_new ("Command:");
+  gtk_label_set_justify (GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+/*    gtk_misc_set_padding (GTK_MISC(label),20,2); */
+  gtk_box_pack_start (GTK_BOX(hbox),label,TRUE,TRUE,2);
+  gtk_widget_show(label);
+  hbox2 = gtk_hbox_new (FALSE,0);
+  gtk_box_pack_start (GTK_BOX(hbox),hbox2,TRUE,TRUE,0);
+  gtk_widget_show (hbox2);
+  entry = gtk_entry_new_with_max_length (MAXCMDLEN-1);
+  info->dnj.ecmd = entry;
+  gtk_box_pack_start (GTK_BOX(hbox2),entry,TRUE,TRUE,2);
+  gtk_widget_show(entry);
+  button = gtk_button_new_with_label ("Search");
+  gtk_box_pack_start (GTK_BOX(hbox2),button,FALSE,FALSE,2);
+  gtk_widget_show (button);
+  gtk_signal_connect (GTK_OBJECT(button),"clicked",dnj_psearch,&info->dnj);
+
+  /* Start and End frames */
+  hbox = gtk_hbox_new (TRUE,2);
+  gtk_box_pack_start (GTK_BOX(vbox),hbox,FALSE,FALSE,2);
+  gtk_widget_show (hbox);
+  label = gtk_label_new ("Start frame:");
+  gtk_label_set_justify (GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+  gtk_box_pack_start (GTK_BOX(hbox),label,FALSE,FALSE,2);
+  gtk_widget_show(label);
+  entry = gtk_entry_new ();
+  gtk_entry_set_text (GTK_ENTRY(entry),"1");
+  info->dnj.esf = entry;
+  gtk_box_pack_start (GTK_BOX(hbox),entry,TRUE,TRUE,2);
+  gtk_widget_show(entry);
+  hbox = gtk_hbox_new (TRUE,2);
+  gtk_box_pack_start (GTK_BOX(vbox),hbox,FALSE,FALSE,2);
+  gtk_widget_show (hbox);
+  label = gtk_label_new ("End frame:");
+  gtk_label_set_justify (GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+  gtk_box_pack_start (GTK_BOX(hbox),label,FALSE,FALSE,2);
+  gtk_widget_show(label);
+  entry = gtk_entry_new ();
+  gtk_entry_set_text (GTK_ENTRY(entry),"100");
+  info->dnj.eef = entry;
+  gtk_box_pack_start (GTK_BOX(hbox),entry,TRUE,TRUE,2);
+  gtk_widget_show(entry);
+
+  /* Priority */
+  hbox = gtk_hbox_new (TRUE,2);
+  gtk_box_pack_start (GTK_BOX(vbox),hbox,FALSE,FALSE,2);
+  gtk_widget_show (hbox);
+  label = gtk_label_new ("Priority:");
+  gtk_label_set_justify (GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+  gtk_box_pack_start (GTK_BOX(hbox),label,TRUE,TRUE,2);
+  gtk_widget_show(label);
+  hbox2 = gtk_hbox_new (FALSE,0);
+  gtk_box_pack_start (GTK_BOX(hbox),hbox2,TRUE,TRUE,0);
+  gtk_widget_show (hbox2);
+  items = g_list_append (items,"Highest");
+  items = g_list_append (items,"High");
+  items = g_list_append (items,"Normal");
+  items = g_list_append (items,"Low");
+  items = g_list_append (items,"Lowest");
+  items = g_list_append (items,"Custom");
+  combo = gtk_combo_new();
+  gtk_combo_set_popdown_strings (GTK_COMBO(combo),items);
+  gtk_widget_show (combo);
+  gtk_box_pack_start (GTK_BOX(hbox2),combo,TRUE,TRUE,0);
+  gtk_entry_set_editable (GTK_ENTRY(GTK_COMBO(combo)->entry),FALSE);
+  info->dnj.cpri = combo;
+  entry = gtk_entry_new_with_max_length (MAXCMDLEN-1);
+  info->dnj.epri = entry;
+  gtk_box_pack_start (GTK_BOX(hbox2),entry,TRUE,TRUE,2);
+  gtk_widget_show(entry);
+  gtk_signal_connect (GTK_OBJECT(GTK_ENTRY(GTK_COMBO(combo)->entry)),
+		      "changed",dnj_cpri_changed,&info->dnj);
+  gtk_entry_set_text (GTK_ENTRY(GTK_COMBO(combo)->entry),"Normal");
+
+  /* Buttons */
+  /* submit */
+  bbox = gtk_hbutton_box_new ();
+  gtk_hbutton_box_set_layout_default (GTK_BUTTONBOX_SPREAD);
+  gtk_box_pack_start (GTK_BOX(vbox),bbox,TRUE,TRUE,5);
+  gtk_widget_show (bbox);
+  button = gtk_button_new_with_label ("Submit");
+  gtk_box_pack_start (GTK_BOX(bbox),button,TRUE,TRUE,2);
+  gtk_widget_show (button);
+  gtk_signal_connect (GTK_OBJECT(button),"clicked",
+		      dnj_bsubmit_pressed,&info->dnj);
+
+  /* cancel */
+  button = gtk_button_new_with_label ("Cancel");
+  gtk_box_pack_start (GTK_BOX(bbox),button,TRUE,TRUE,2);
+  gtk_widget_show (button);
+  gtk_signal_connect_object (GTK_OBJECT(button),"clicked",
+			     GTK_SIGNAL_FUNC(gtk_widget_destroy),
+			     (gpointer) window);
+
+  gtk_widget_show (vbox);
+  gtk_widget_show(window);
+
+  return window;
+}
+
+static void dnj_psearch (GtkWidget *button, struct info_dnj *info)
+{
+  GtkWidget *dialog;
+
+  dialog = gtk_file_selection_new ("Please select a file as job command");
+  info->fs = dialog;
+
+  gtk_signal_connect (GTK_OBJECT (GTK_FILE_SELECTION(dialog)->ok_button),
+		      "clicked", GTK_SIGNAL_FUNC (dnj_set_cmd), info);
+  gtk_signal_connect_object (GTK_OBJECT (GTK_FILE_SELECTION(dialog)->ok_button),
+			     "clicked", GTK_SIGNAL_FUNC (gtk_widget_destroy),
+			     (gpointer) dialog);
+  gtk_signal_connect_object (GTK_OBJECT (GTK_FILE_SELECTION(dialog)->cancel_button),
+			     "clicked", GTK_SIGNAL_FUNC (gtk_widget_destroy),
+			     (gpointer) dialog);
+  gtk_widget_show (dialog);
+  gtk_window_set_modal (GTK_WINDOW(dialog),TRUE);
+}
+
+static void dnj_set_cmd (GtkWidget *button, struct info_dnj *info)
+{
+  gtk_entry_set_text (GTK_ENTRY(info->ecmd),gtk_file_selection_get_filename(GTK_FILE_SELECTION(info->fs)));
+}
+
+static void dnj_cpri_changed (GtkWidget *entry, struct info_dnj *info)
+{
+  if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"Highest") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),FALSE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"100");
+  } else if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"High") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),FALSE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"250");
+  } else if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"Normal") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),FALSE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"500");
+  } else if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"Low") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),FALSE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"750");
+  } else if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"Lowest") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),FALSE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"1000");
+  } else if (strcmp(gtk_entry_get_text(GTK_ENTRY(entry)),"Custom") == 0) {
+    gtk_entry_set_editable (GTK_ENTRY(info->epri),TRUE);
+    gtk_entry_set_text (GTK_ENTRY(info->epri),"500");
+  } else {
+    fprintf (stderr,"Not listed!\n");
+  }
+}
+
+static void dnj_bsubmit_pressed (GtkWidget *button, struct info_dnj *info)
+{
+  GtkWidget *dialog, *label, *okay_button;
+
+  if (!dnj_submit(info)) {
+    dialog = gtk_dialog_new();
+    gtk_window_set_modal (GTK_WINDOW(dialog),TRUE);
+    label = gtk_label_new ("The information is not correct or master not available.\n Check it, please.");
+    okay_button = gtk_button_new_with_label("Ok");
+
+    gtk_signal_connect_object (GTK_OBJECT (okay_button), "clicked",
+			       GTK_SIGNAL_FUNC (gtk_widget_destroy),(GtkObject*)dialog);
+    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->action_area),
+		       okay_button);
+    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox),
+                      label);
+
+    gtk_widget_show_all (dialog);
+  } else {
+    gtk_widget_destroy (info->dialog);
+  }
+}
+
+static int dnj_submit (struct info_dnj *info)
+{
+  /* This is the function that actually submits the job info */
+  struct job job;
+
+  strncpy(job.name,gtk_entry_get_text(GTK_ENTRY(info->ename)),MAXNAMELEN-1);
+  if (strlen(job.name) == 0)
+    return 0;
+  strncpy(job.cmd,gtk_entry_get_text(GTK_ENTRY(info->ecmd)),MAXCMDLEN-1);
+  if (strlen(job.cmd) == 0)
+    return 0;
+  if (sscanf(gtk_entry_get_text(GTK_ENTRY(info->esf)),"%u",&job.frame_start) != 1)
+    return 0;
+  if (sscanf(gtk_entry_get_text(GTK_ENTRY(info->eef)),"%u",&job.frame_end) != 1)
+    return 0;
+  if (sscanf(gtk_entry_get_text(GTK_ENTRY(info->epri)),"%u",&job.priority) != 1)
+    return 0;
+
+  strncpy (job.owner,getlogin(),MAXNAMELEN-1);
+  job.owner[MAXNAMELEN-1] = 0;
+  job.status = JOBSTATUS_WAITING;
+  job.frame_info = NULL;
+
+  if (!register_job (&job))
+    return 0;
+
+  return 1;
+}
 
 
 
