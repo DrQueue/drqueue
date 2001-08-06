086048c839d87d44395131cf536bdffafab6a95e
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-08-06 12:33:51 +0000

    Added: DRE_ANSWERNOTLISTED, DRE_ANSWERNOTRIGHT, DRE_ERRORSENDING.

--- drerrno.c
+++ drerrno.c
@@ -1,4 +1,4 @@
-/* $Id: drerrno.c,v 1.1 2001/05/28 14:21:31 jorge Exp $ */
+/* $Id: drerrno.c,v 1.2 2001/08/06 12:33:50 jorge Exp $ */
 
 #include "drerrno.h"
 #include "constants.h"
@@ -11,14 +11,23 @@ char *drerrno_str (void)
   case DRE_NODRMAENV:
     msg = "DRQUEUE_MASTER not defined on environment";
     break;
-  case DRE_DRMANONVA:
-    msg = "DRQUEUE_MASTER pointing to a non-valid address";
+  case DRE_NOTRESOLV:
+    msg = "Could not resolv hostname";
     break;
   case DRE_NOSOCKET:
     msg = "Could not open socket";
     break;
   case DRE_NOCONNECT:
     msg = "Could not connect to master";
+    break;
+  case DRE_ANSWERNOTLISTED:
+    msg = "Received request answer value not listed";
+    break;
+  case DRE_ANSWERNOTRIGHT:
+    msg = "Received request answer value not appropiate for this query";
+    break;
+  case DRE_ERRORSENDING:
+    msg = "Error sending through socket";
     break;
   default:
     msg = "drerrno NOT listed !!";
