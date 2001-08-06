b347e58b6e88931546f956bdee0f3cccef294f1e
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-08-06 12:31:38 +0000

    Minor changes on logging (moved to L_DEBUG). Check if the job is being used and is the same in handle_r_r_taskfini. Added request_job_delete. Added handle_r_r_deletjob. Added request_slave_killtask. Added handle_rs_r_killtask.

--- drqueue/request.h
+++ drqueue/request.h
@@ -1,4 +1,4 @@
-/* $Id: request.h,v 1.10 2001/08/02 10:19:04 jorge Exp $ */
+/* $Id: request.h,v 1.11 2001/08/06 12:31:38 jorge Exp $ */
 /* The request structure is not just used for the requests themselves */
 /* but also for the answers to the requests */
 
@@ -23,13 +23,14 @@
 
 struct request {
   unsigned char type;		/* Kind of request */
-  unsigned char slave;		/* Is the requester a slave ? For updating lastconn time */
+  unsigned char who;		/* who sends this request, a master, a client, a slave... (constants.h) *
   uint16_t data_s;		/* Data number that might be needed for the request */
 };
 
 void handle_request_master (int sfd,struct database *wdb,int icomp);
 void handle_request_slave (int sfd,struct slave_database *sdb);
 
+/* MASTER */
 void handle_r_r_register (int sfd,struct database *wdb,int icomp);
 void handle_r_r_ucstatus (int sfd,struct database *wdb,int icomp);
 void handle_r_r_regisjob (int sfd,struct database *wdb);
@@ -37,13 +38,22 @@ void handle_r_r_availjob (int sfd,struct database *wdb,int icomp);
 void handle_r_r_taskfini (int sfd,struct database *wdb,int icomp);
 void handle_r_r_listjobs (int sfd,struct database *wdb,int icomp);
 void handle_r_r_listcomp (int sfd,struct database *wdb,int icomp);
+void handle_r_r_deletjob (int sfd,struct database *wdb,int icomp);
 
+/* TO MASTER */
 void update_computer_status (struct computer *computer); /* The slave calls this function to update the */
                                                         /* information that the master has about him */
 void register_slave (struct computer *computer);
 int register_job (struct job *job);
 int request_job_available (struct slave_database *sdb);
 void request_task_finished (struct slave_database *sdb);
+int request_job_delete (struct job *job,int who);
+
+/* TO SLAVE */
+int request_slave_killtask (char *slave,uint16_t itask);
+
+/* SLAVE */
+void handle_rs_r_killtask (int sfd,struct slave_database *sdb,struct request *req);
 
 #endif /* _REQUEST_H_ */
 
