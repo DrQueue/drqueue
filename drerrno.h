8968287d99ef0418da0bdb323c68c2411c3de29a
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-08-06 12:33:51 +0000

    Added: DRE_ANSWERNOTLISTED, DRE_ANSWERNOTRIGHT, DRE_ERRORSENDING.
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@126 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drerrno.h
+++ drerrno.h
@@ -1,14 +1,17 @@
-/* $Id: drerrno.h,v 1.1 2001/05/28 14:21:31 jorge Exp $ */
+/* $Id: drerrno.h,v 1.2 2001/08/06 12:33:51 jorge Exp $ */
 
 #ifndef _DRERRNO_H_
 #define _DRERRNO_H_
 
 int drerrno;			/* global errno */
 
-#define DRE_NODRMAENV 1		/* No DRQUEUE_MASTER on the environment */
-#define DRE_DRMANONVA 2		/* DRQUEUE_MASTER non valid */
-#define DRE_NOSOCKET  3		/* Could not open socket */
-#define DRE_NOCONNECT 4		/* Could not connect */
+#define DRE_NODRMAENV       1	/* No DRQUEUE_MASTER on the environment */
+#define DRE_NOTRESOLV       2	/* Could not resolve hostname */
+#define DRE_NOSOCKET        3	/* Could not open socket */
+#define DRE_NOCONNECT       4	/* Could not connect */
+#define DRE_ANSWERNOTLISTED 5	/* Answer received not listed */
+#define DRE_ANSWERNOTRIGHT  6	/* Not the right answer to this request */
+#define DRE_ERRORSENDING    7	/* Error sending :) */
 
 char *drerrno_str (void);
 
