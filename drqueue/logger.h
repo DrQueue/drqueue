c45383488c7a1f402b7cde576bc5feeb1ba5ee0b
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-08-07 13:00:33 +0000

    Added global loglevel.

--- drqueue/logger.h
+++ drqueue/logger.h
@@ -1,4 +1,4 @@
-/* $Id: logger.h,v 1.7 2001/07/20 15:27:34 jorge Exp $ */
+/* $Id: logger.h,v 1.8 2001/08/07 13:00:33 jorge Exp $ */
 
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
@@ -9,12 +9,12 @@
 #include "job.h"
 #include "computer.h"
 
-#define L_CURRENT 2
-
 #define L_ERROR 0
 #define L_WARNING 1
 #define L_INFO 2
 #define L_DEBUG 3
+
+int loglevel = L_WARNING;
 
 void log_slave_task (struct task *task, int level, char *msg);
 FILE *log_slave_open_task (struct task *task);
