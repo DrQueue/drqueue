25630c4d7ca7c0652f838264b8f725a777158301
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-08-27 15:16:54 +0000

    Added: * struct info_djd
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@173 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqman/drqm_jobs.h
+++ drqman/drqm_jobs.h
@@ -1,5 +1,5 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/drqm_jobs.h,v 1.8 2001/08/23 13:24:28 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/drqm_jobs.h,v 1.9 2001/08/27 15:16:54 jorge Exp $
  */
 
 #ifndef _DRQM_JOBS_H_
@@ -12,9 +12,21 @@ struct info_dnj {		/* dialog new job */
   GtkWidget *dialog;
   GtkWidget *ename;
   GtkWidget *ecmd;
-  GtkWidget *esf,*eef;		/* entry start frame, entry end frame */
+  GtkWidget *esf,*eef,*estf;	/* entry start frame, entry end frame, entry step frames */
   GtkWidget *cpri,*epri;	/* combo priority, entry priority */
   GtkWidget *fs;		/* File selector */
+};
+
+struct info_djd {		/* dialog job details */
+  GtkWidget *dialog;
+  GtkWidget *lname;		/* label name */
+  GtkWidget *lcmd;		/* label command */
+  GtkWidget *lstartend;		/* start and end frames */
+  GtkWidget *lpri;		/* priority */
+  GtkWidget *lfrldf;		/* frames left, done and failed */
+  GtkWidget *lavgt;		/* average time per frame */
+  GtkWidget *lestf;		/* estimated finish time */
+  GtkWidget *clist;		/* frame info clist */
 };
 
 struct info_drqm_jobs {
@@ -26,6 +38,7 @@ struct info_drqm_jobs {
   uint32_t njobs;		/* Number of jobs in the list */
   struct job *jobs;		/* The job list */
   struct info_dnj dnj;		/* dialog new job */
+  struct info_djd djd;		/* dialog job details */
 };
 
 void CreateJobsPage (GtkWidget *);
