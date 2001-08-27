dff15045597749742ce42048f21956e86916a19b
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-08-27 08:21:11 +0000

    Request data_s (short) moved to data (int [uint32_t]). Added handle_r_r_jobxfer. Added handle_r_r_jobxferfi. Added request_job_xfer. Added request_job_xferfi.

--- drqueue/request.h
+++ drqueue/request.h
@@ -1,4 +1,4 @@
-/* $Id: request.h,v 1.14 2001/08/08 12:59:46 jorge Exp $ */
+/* $Id: request.h,v 1.15 2001/08/27 08:21:11 jorge Exp $ */
 /* The request structure is not just used for the requests themselves */
 /* but also for the answers to the requests */
 
@@ -24,7 +24,7 @@
 struct request {
   unsigned char type;		/* Kind of request */
   unsigned char who;		/* who sends this request, a master, a client, a slave... (constants.h) */
-  uint16_t data_s;		/* Data number that might be needed for the request */
+  uint32_t data;		/* Data number that might be needed for the request */
 };
 
 void handle_request_master (int sfd,struct database *wdb,int icomp);
@@ -42,6 +42,8 @@ void handle_r_r_deletjob (int sfd,struct database *wdb,int icomp);
 void handle_r_r_stopjob  (int sfd,struct database *wdb,int icomp,struct request *req);
 void handle_r_r_contjob  (int sfd,struct database *wdb,int icomp,struct request *req);
 void handle_r_r_hstopjob (int sfd,struct database *wdb,int icomp,struct request *req);
+void handle_r_r_jobxfer (int sfd,struct database *wdb,int icomp,struct request *req);
+void handle_r_r_jobxferfi (int sfd,struct database *wdb,int icomp,struct request *req);
 
 /* TO MASTER */
 void update_computer_status (struct computer *computer); /* The slave calls this function to update the */
@@ -54,7 +56,8 @@ int request_job_delete (struct job *job,int who);
 int request_job_stop (uint32_t ijob, int who);
 int request_job_hstop (uint32_t ijob, int who);
 int request_job_continue (uint32_t ijob, int who);
-
+int request_job_xfer (uint32_t ijob, struct job *job, int who);
+int request_job_xferfi (uint32_t ijob, struct frame_info *fi, int nframes, int who);
 
 /* TO SLAVE */
 int request_slave_killtask (char *slave,uint16_t itask);
