9c864c08dc2cce4b3fcbe163aaf31901272f356f
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-08-28 21:49:29 +0000

    Globlal info_drqm in progress.
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@199 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqman/drqm_computers.h
+++ drqman/drqm_computers.h
@@ -1,5 +1,5 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/drqm_computers.h,v 1.1 2001/07/19 09:22:25 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/drqm_computers.h,v 1.2 2001/08/28 21:49:26 jorge Exp $
  */
 
 #ifndef _DRQM_COMPUTERS_H
@@ -8,15 +8,29 @@
 #include <gtk/gtk.h>
 #include "libdrqueue.h"
 
+struct info_cdd {		/* computer details dialog */
+  GtkWidget *dialog;
+  GtkWidget *lname;		/* label name */
+
+  GtkWidget *menu;		/* Popup menu */
+  gint row,column;		/* selected processor */
+  int selected;			/* if a processor is selected */
+};
+
 struct info_drqm_computers {
   GtkWidget *clist;		/* main clist */
   gint row, column;
   GtkWidget *menu;		/* Popup menu */
+  int selected;			/* if a computer is selected */
   uint32_t ncomputers;		/* Number of computers in the list */
-  struct computer *computers;		/* The computer list */
+  uint32_t icomp;		/* id of the selected computer */
+  struct computer *computers;	/* The computer list */
+  struct info_cdd cdd;
 };
 
-void CreateComputersPage (GtkWidget *);
+struct info_drqm;
+
+void CreateComputersPage (GtkWidget *notebook,struct info_drqm *info);
 void drqm_update_computerlist (struct info_drqm_computers *info);
 
 #endif /* _DRQM_COMPUTERS_H */
--- drqman/notebook.c
+++ drqman/notebook.c
@@ -1,5 +1,5 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/notebook.c,v 1.2 2001/07/19 09:09:15 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/notebook.c,v 1.3 2001/08/28 21:49:28 jorge Exp $
  */
 
 #include <gtk/gtk.h>
@@ -7,7 +7,7 @@
 #include "drqm_jobs.h"
 #include "drqm_computers.h"
 
-void CreateNotebook (GtkWidget *window,GtkWidget *vbox)
+void CreateNotebook (GtkWidget *window,GtkWidget *vbox,struct info_drqm *info)
 {
   GtkWidget *notebook;
 
@@ -15,7 +15,7 @@ void CreateNotebook (GtkWidget *window,GtkWidget *vbox)
   gtk_notebook_set_tab_pos(GTK_NOTEBOOK(notebook),GTK_POS_TOP);
   
   CreateJobsPage(notebook);
-  CreateComputersPage(notebook);
+  CreateComputersPage(notebook,info);
   
   gtk_box_pack_end(GTK_BOX(vbox), notebook, TRUE,TRUE, 0);
 
