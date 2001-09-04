04cc29c9c9ad7d98b9578e4c263e8550496cf534
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-04 23:24:29 +0000

    Small update.

--- drqueue/mayasg.h
+++ drqueue/mayasg.h
@@ -1,16 +1,16 @@
-/* $Id: mayasg.h,v 1.1 2001/09/04 12:30:33 jorge Exp $ */
+/* $Id: mayasg.h,v 1.2 2001/09/04 23:24:29 jorge Exp $ */
 
 #ifndef _MAYASG_H_
 #define _MAYASG_H_
 
 #include "constants.h"
 
-struct mayasg {
+struct mayasgi {		/* Maya Script Generator Info */
   char project[BUFFERLEN];
   char scene[BUFFERLEN];
   char image[BUFFERLEN];
 };
 
-char *mayasg_create (struct mayasg *info);
+char *mayasg_create (struct mayasgi *info);
 
 #endif /* _MAYASG_H_ */
