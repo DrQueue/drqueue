92c20bc21b6bc47dad0fc587417da2327b4f041d
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-05 15:18:05 +0000

    Added show_version. Added VERSION define.

--- common.h
+++ common.h
@@ -1,8 +1,13 @@
-/* $Id: common.h,v 1.1 2001/09/02 12:18:53 jorge Exp $ */
+/* $Id: common.h,v 1.2 2001/09/05 15:18:05 jorge Exp $ */
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
+#include <stdio.h>
+
+#define VERSION "v_0-19"
+
 int common_environment_check (void);
+void show_version (char **argv);
 
 #endif /* _COMMON_H_ */
