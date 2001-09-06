89ef79553196696860b876939c51de31f80dc72a
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-09-06 14:33:36 +0000

    Added drqm_request_job_frame_finish.
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@299 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqman/drqm_request.h
+++ drqman/drqm_request.h
@@ -1,4 +1,4 @@
-/* $Id: drqm_request.h,v 1.7 2001/09/06 10:19:43 jorge Exp $ */
+/* $Id: drqm_request.h,v 1.8 2001/09/06 14:33:36 jorge Exp $ */
 
 #ifndef _DRQM_REQUEST_H_
 #define _DRQM_REQUEST_H_
@@ -14,6 +14,7 @@ void drqm_request_job_hstop (struct drqm_jobs_info *info);
 void drqm_request_job_continue (struct drqm_jobs_info *info);
 void drqm_request_job_frame_waiting (uint32_t jobid,uint32_t frame);
 void drqm_request_job_frame_kill (uint32_t jobid,uint32_t frame);
+void drqm_request_job_frame_finish (uint32_t jobid,uint32_t frame);
 
 void drqm_request_computerlist (struct info_drqm_computers *info);
 void clean_computerlist (struct info_drqm_computers *info);
