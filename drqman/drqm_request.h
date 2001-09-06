c51ae4a77206eaec605dd2179301fa14ecae0999
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-06 23:03:26 +0000

    Added drqm_request_job_frame_kill_finish.

--- drqman/drqm_request.h
+++ drqman/drqm_request.h
@@ -1,4 +1,4 @@
-/* $Id: drqm_request.h,v 1.8 2001/09/06 14:33:36 jorge Exp $ */
+/* $Id: drqm_request.h,v 1.9 2001/09/06 23:03:26 jorge Exp $ */
 
 #ifndef _DRQM_REQUEST_H_
 #define _DRQM_REQUEST_H_
@@ -15,6 +15,7 @@ void drqm_request_job_continue (struct drqm_jobs_info *info);
 void drqm_request_job_frame_waiting (uint32_t jobid,uint32_t frame);
 void drqm_request_job_frame_kill (uint32_t jobid,uint32_t frame);
 void drqm_request_job_frame_finish (uint32_t jobid,uint32_t frame);
+void drqm_request_job_frame_kill_finish (uint32_t jobid,uint32_t frame);
 
 void drqm_request_computerlist (struct info_drqm_computers *info);
 void clean_computerlist (struct info_drqm_computers *info);
