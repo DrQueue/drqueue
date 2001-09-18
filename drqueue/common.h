4ee2d6de275aaeaf023029fdde6a44a2066e3272
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-18 10:45:44 +0000

    Version update.

--- drqueue/common.h
+++ drqueue/common.h
@@ -1,11 +1,11 @@
-/* $Id: common.h,v 1.5 2001/09/17 12:36:14 jorge Exp $ */
+/* $Id: common.h,v 1.6 2001/09/18 10:45:44 jorge Exp $ */
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
 #include <stdio.h>
 
-#define VERSION "v_0-22"
+#define VERSION "v_0-23"
 
 int common_environment_check (void);
 void show_version (char **argv);
