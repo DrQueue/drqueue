655fe534af08e997c6b9258afe5d21474a8525f0
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-18 09:24:11 +0000

    First commit.

---
+++ drqueue/drqman/drqm_common.h
@@ -0,0 +1,21 @@
+/*
+ * $Header: /root/cvs/drqueue/drqman/drqm_common.h,v 1.1 2001/09/18 09:24:11 jorge Exp $
+ */
+
+#ifndef _DRQM_COMMON_H_
+#define _DRQM_COMMON_H_
+
+#include <gtk/gtk.h>
+#include "libdrqueue.h"
+
+GtkWidget *ConfirmDialog (char *text, GList *callbacks, gpointer data);
+
+#endif /* _DRQM_COMMON_H */
+
+
+
+
+
+
+
+
