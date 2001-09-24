d23f23aec62648368593225571facf9caf166e35
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-09-24 16:10:20 +0000

    Added :  * TooltipsNew : that creates a new tooltips object and sets the delay as TOOLTIPS_DELAY

--- drqueue/drqman/drqm_common.h
+++ drqueue/drqman/drqm_common.h
@@ -1,5 +1,5 @@
 /*
- * $Header: /root/cvs/drqueue/drqman/drqm_common.h,v 1.1 2001/09/18 09:24:11 jorge Exp $
+ * $Header: /root/cvs/drqueue/drqman/drqm_common.h,v 1.2 2001/09/24 16:10:20 jorge Exp $
  */
 
 #ifndef _DRQM_COMMON_H_
@@ -8,7 +8,10 @@
 #include <gtk/gtk.h>
 #include "libdrqueue.h"
 
+#define TOOLTIPS_DELAY 1000
+
 GtkWidget *ConfirmDialog (char *text, GList *callbacks, gpointer data);
+GtkTooltips *TooltipsNew (void);
 
 #endif /* _DRQM_COMMON_H */
 
