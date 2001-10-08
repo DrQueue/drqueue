c378019e1436385a8c194648ccf8a84863b32c65
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-10-08 15:02:43 +0000

    Updated
    git-svn-id: https://ssl.drqueue.org/svn/trunk@459 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/common.h
+++ drqueue/common.h
@@ -1,11 +1,11 @@
-/* $Id: common.h,v 1.11 2001/10/08 12:33:18 jorge Exp $ */
+/* $Id: common.h,v 1.12 2001/10/08 15:02:43 jorge Exp $ */
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
 #include <stdio.h>
 
-#define VERSION "v_0-28"
+#define VERSION "v_0-29"
 
 int common_environment_check (void);
 void show_version (char **argv);
