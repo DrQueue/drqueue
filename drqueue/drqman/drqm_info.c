171a7bd8a392fdc0b68394f918bf0672e4853446
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2001-11-08 11:47:43 +0000

    First logo inside the info view.
    git-svn-id: https://ssl.drqueue.org/svn/trunk@489 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drqman/drqm_info.c
+++ drqueue/drqman/drqm_info.c
@@ -1,5 +1,5 @@
 /*
- * $Id: drqm_info.c,v 1.1 2001/11/08 11:10:20 jorge Exp $
+ * $Id: drqm_info.c,v 1.2 2001/11/08 11:47:41 jorge Exp $
  */
 
 #include <string.h>
@@ -8,13 +8,14 @@
 #include <pwd.h>
 #include <sys/types.h>
 
-#include "waiting.xpm"
-#include "running.xpm"
-#include "finished.xpm"
-#include "error.xpm"
+#include "eeelogo.xpm"
 
 #include "drqman.h"
 #include "drqm_common.h"
+
+/* Static functions declarations */
+static GtkWidget *CreateLogoWidget (GtkWidget *widget);
+
 
 void CreateInfoPage (GtkWidget *notebook, struct info_drqm *info)
 {
@@ -22,6 +23,7 @@ void CreateInfoPage (GtkWidget *notebook, struct info_drqm *info)
   GtkWidget *label;
   GtkWidget *container;
   GtkWidget *vbox;
+  GtkWidget *logo;
 
   /* Label */
   label = gtk_label_new ("Info");
@@ -30,13 +32,28 @@ void CreateInfoPage (GtkWidget *notebook, struct info_drqm *info)
   vbox = gtk_vbox_new(FALSE,2);
   gtk_container_add(GTK_CONTAINER(container),vbox);
 
+  logo = CreateLogoWidget (info->main_window);
+
+  gtk_box_pack_start (GTK_BOX(vbox),logo,FALSE,FALSE,2);
+
   /* Append the page */
   gtk_notebook_append_page (GTK_NOTEBOOK(notebook), container, label);
 
-  gtk_widget_show(vbox);
+  gtk_widget_show_all(vbox);
   gtk_widget_show(label);
   gtk_widget_show(container);
 }
 
+GtkWidget *CreateLogoWidget (GtkWidget *widget)
+{
+  GtkWidget *toplevel;
+  GdkBitmap *l_mask = NULL; /* Logo mask */
+  GdkPixmap *l_data = NULL; /* Logo data */
 
+  toplevel = gtk_widget_get_toplevel(widget);
+  gtk_widget_realize(toplevel);	/* Just in case */
+  l_data = gdk_pixmap_create_from_xpm_d (GTK_WIDGET(toplevel)->window,&l_mask,NULL,(gchar**)eeelogo_xpm);
+
+  return gtk_pixmap_new(l_data,l_mask);
+}
 
