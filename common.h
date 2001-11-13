225a5a457a985b08ca953eeed6696c0da3cc4b98
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2001-11-13 15:50:25 +0000

    Added :  * time_str : function to convert a number of seconds into an string with hours, minutes and seconds.

--- common.h
+++ common.h
@@ -1,14 +1,28 @@
-/* $Id: common.h,v 1.14 2001/10/29 10:13:56 jorge Exp $ */
+/* $Id: common.h,v 1.15 2001/11/13 15:50:25 jorge Exp $ */
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-#include <stdio.h>
+#ifdef __LINUX
+#include <stdint.h>
+#else
+# ifdef __IRIX
+#include <sys/types.h>
+# else
+#  error You need to define the OS, or OS defined not supported
+# endif
+#endif
+
+#include "job.h"
 
 #define VERSION "v_0-30"
 
 int common_environment_check (void);
 void show_version (char **argv);
 int remove_dir (char *dir);
+char *time_str (uint32_t nseconds);
+
+/* Mail notifications */
+void mn_job_finished (struct job *job);
 
 #endif /* _COMMON_H_ */
