b26c7154c2750e2fdbd9033718b451b1eb9a69f6
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2002-03-01 11:06:53 +0000

    Test comment
    git-svn-id: https://ssl.drqueue.org/svn/trunk@561 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/logger.c
+++ drqueue/logger.c
@@ -1,4 +1,4 @@
-/* $Id: logger.c,v 1.23 2002/03/01 11:00:04 jorge Exp $ */
+/* $Id: logger.c,v 1.24 2002/03/01 11:06:53 jorge Exp $ */
 
 #include <unistd.h>
 #include <stdio.h>
@@ -35,6 +35,8 @@ void log_slave_task (struct task *task,int level,char *fmt,...)
 
   if (level > loglevel)
     return;
+
+  /* Test comment */
 
   va_start (ap,fmt);
   vsnprintf (msg,BUFFERLEN-1,fmt,ap);
