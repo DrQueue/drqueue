cd80223b63f803c9f494bec108fa1152a5e1bad3
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2004-10-09 05:21:43 +0000

    New sendjob supporting Maya and Blender

--- drqueue/sendjob.h
+++ drqueue/sendjob.h
@@ -1,4 +1,4 @@
-/* $Id: sendjob.h,v 1.4 2004/10/06 16:26:53 jorge Exp $ */
+/* $Id: sendjob.h,v 1.5 2004/10/09 05:21:28 jorge Exp $ */
 
 #ifndef _SENDJOB_H_
 #define _SENDJOB_H_
@@ -6,9 +6,16 @@
 #include <fstream>
 #include "job.h"
 
+#define TOJ_NONE		0
+#define TOJ_MAYA		1
+#define	TOJ_BLENDER	2
+
 void presentation (void);
 
-int RegisterJobFromFile (std::ifstream &infile);
+int RegisterMayaJobFromFile (std::ifstream &infile);
+int RegisterBlenderJobFromFile (std::ifstream &infile);
+
+int str2toj (char *str);
 
 void cleanup (int signum);
 
