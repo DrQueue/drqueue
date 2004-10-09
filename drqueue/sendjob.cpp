256c0e37b55669f9ee1dc5eca315956471a57b84
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2004-10-09 05:21:43 +0000

    New sendjob supporting Maya and Blender
    git-svn-id: https://ssl.drqueue.org/svn/trunk@683 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/sendjob.cpp
+++ drqueue/sendjob.cpp
@@ -1,5 +1,5 @@
 //
-// $Id: sendjob.cpp,v 1.4 2004/10/06 16:26:53 jorge Exp $
+// $Id: sendjob.cpp,v 1.5 2004/10/09 05:21:43 jorge Exp $
 // 
 // To set up a maya job from a file and send it to the master
 //
@@ -25,14 +25,18 @@
 int main (int argc,char *argv[])
 {
   int opt;
+	int toj = 0;
     
   presentation();
 
-  while ((opt = getopt (argc,argv,"hv")) != -1) {
+  while ((opt = getopt (argc,argv,"hvt:")) != -1) {
     switch (opt) {
     case 'v':
       //      show_version (argv);
       exit (0);
+		case 't':
+			toj = str2toj (optarg);
+			break;
     case '?':
     case 'h':
       usage();
@@ -40,21 +44,29 @@ int main (int argc,char *argv[])
     }
   }
 
-  if (argc < 2) {
+  if ((argc < 2) || (toj == TOJ_NONE)) {
     usage ();
     exit (1);
   }
 
   
   std::ifstream infile(argv[1]);
-  if (RegisterJobFromFile (infile)) {
-		std::cerr << "Error register job from file: " << argv[1] << std::endl;
-    exit (1);
-  }
+	switch (toj) {
+		case TOJ_MAYA:
+  		if (RegisterMayaJobFromFile (infile)) {
+				std::cerr << "Error registering MAYA job from file: " << argv[1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_BLENDER:
+  		if (RegisterBlenderJobFromFile (infile)) {
+				std::cerr << "Error registering BLENDER job from file: " << argv[1] << std::endl;
+    		exit (1);
+  		}
+			break;
+	}
 
   std::cerr << "Job sent successfuly to the queue\n";
-
-  //  register_job (&job);
 
   exit (0);
 }
@@ -71,13 +83,14 @@ void cleanup (int signum)
 
 void usage (void)
 {
-  std::cerr << "Usage: sendjob [-vh] <job_file>\n"
+  std::cerr << "Usage: sendjob [-vh] -t <type> <job_file>\n"
 						<< "Valid options:\n"
 						<< "\t-v version information\n"
-						<< "\t-h prints this help\n";
+						<< "\t-h prints this help\n"
+						<< "\t-t [maya|blender] type of job";
 }
 
-int RegisterJobFromFile (std::ifstream &infile)
+int RegisterMayaJobFromFile (std::ifstream &infile)
 {
   // Job variables for the script generator
   struct job job;
@@ -150,8 +163,68 @@ int RegisterJobFromFile (std::ifstream &infile)
   return 0;
 }
 
+int RegisterBlenderJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct blendersgi blenderSgi;
 
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
 
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	// 
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
 
+  strncpy(blenderSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(blenderSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
 
+  if (!(pathToScript = blendersg_create(&blenderSgi))) {
+		std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,getenv("USER"),MAXNAMELEN-1);
+  strncpy (job.email,getenv("USER"),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.block_size = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_BLENDER;
+  strncpy (job.koji.blender.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.blender.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = -1;
+  job.limits.nmaxcpuscomputer = -1;
+
+  if (!register_job(&job)) {
+		std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int str2toj (char *str)
+{
+	int toj = TOJ_NONE;
+
+	if (strstr(str,"maya") != NULL) {
+		toj = TOJ_MAYA;
+	} else if (strstr(str,"blender") != NULL) {
+		toj = TOJ_BLENDER;
+	}
+
+	return toj;
+}
 
