0d4b0b31e62e07cda31efa93af845e044ab963b0
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2004-10-14 18:32:12 +0000

    Added GPL headers
    git-svn-id: https://ssl.drqueue.org/svn/trunk@771 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drerrno.h
+++ drqueue/drerrno.h
@@ -1,4 +1,22 @@
-/* $Id: drerrno.h,v 1.12 2002/12/02 22:24:08 jorge Exp $ */
+// 
+// Copyright (C) 2001,2002,2003,2004 Jorge Daza Garcia-Blanes
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+// USA
+// 
+/* $Id$ */
 
 #ifndef _DRERRNO_H_
 #define _DRERRNO_H_
