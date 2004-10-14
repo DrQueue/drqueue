1256555cc0854ac36be67d8e4ed49aa8234baa11
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2004-10-14 18:32:12 +0000

    Added GPL headers

--- drqueue/logger.h
+++ drqueue/logger.h
@@ -1,4 +1,22 @@
-/* $Id: logger.h,v 1.13 2002/02/27 10:42:35 jorge Exp $ */
+// 
+// Copyright (C) 2001,2002,2003,2004 Jorge Daza Garcia-Blanes
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+// USA
+// 
+/* $Id$ */
 
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
