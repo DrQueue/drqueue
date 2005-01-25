7b6032bcdc41402d65b25942022fe0a07512558e
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2005-01-25 14:21:45 +0000

    More on cygwin installer

---
+++ drqueue/contrib/service-slave/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de l'ensemble 
+' d'attributs suivant. Pour modifier les informations associées à un assembly, 
+'changez les valeurs de ces attributs.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("29329AFF-4692-4499-98DB-4FDAA30B2948")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+
+<Assembly: AssemblyVersion("1.0.*")> 
