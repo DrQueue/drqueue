dc979f215d7c43f44a51bc7aed704419140d148e
kraken <kraken@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2005-01-27 17:22:58 +0000

    Windows installation fixes (services back for master and ipc)
    git-svn-id: https://ssl.drqueue.org/svn/trunk@1138 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/contrib/windows/build_services.sh
+++ drqueue/contrib/windows/build_services.sh
@@ -3,4 +3,13 @@ START=$PWD
 cd $1/Controller
 $2/csc.exe /t:winexe /win32icon:App.ico /out:ServicesController.exe /resource:ServicesController.Form1.resources *.cs 
 mv ServicesController.exe $1
+cd $1/ServiceInstaller
+gcc -mno-cygwin service.cpp -o service.exe
+mv service.exe $1
+cd $1/Services/Service-ipc
+$2/csc.exe /out:service-ipc.exe *.cs
+mv service-ipc.exe $1
+cd $1/Services/Service-master
+$2/csc.exe /out:service-master.exe *.cs
+mv service-master.exe $1
 cd $START
