f7ff4df6845edcdf8786fec4626728736f680be6
kraken <kraken@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2005-01-31 14:20:01 +0000

    Fixes in windows installation
    git-svn-id: https://ssl.drqueue.org/svn/trunk/drqueue@1142 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- contrib/windows/install_dlls.sh
+++ contrib/windows/install_dlls.sh
@@ -8,20 +8,20 @@ do
 done
 
 #copy dll linked dll
-DLLS=`objdump.exe -p $1/*.exe |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
+DLLS=`objdump.exe -p $1/* |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
 for file in $DLLS;
 do
  cp -v --reply=no "`which $file`" $1;
 done
 
 #copy linked dll linked dll
-DLLS=`objdump.exe -p $1/*.exe |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
+DLLS=`objdump.exe -p $1/* |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
 for file in $DLLS;
  do cp -v --reply=no "`which $file`" $1;
 done
 
 #copy linked dll linked dll
-DLLS=`objdump.exe -p $1/*.exe |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
+DLLS=`objdump.exe -p $1/* |grep "DLL Name" |grep -E -e "(lib|cyg)" |cut -d \  -f 3 |sort -u`
 for file in $DLLS;
  do cp -v --reply=no "`which $file`" $1;
 done
