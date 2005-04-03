15895dcdbfcb285c0f4e3d5adcee602d58ad05ee
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2005-04-03 04:32:02 +0000

    More on the new load balancing algorithm
    git-svn-id: https://ssl.drqueue.org/svn/trunk@1200 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/drqman/drqm_jobs.c
+++ drqueue/drqman/drqm_jobs.c
@@ -983,11 +983,11 @@ static int pri_cmp_clist (GtkCList *clist, gconstpointer ptr1, gconstpointer ptr
   b = (uint32_t) ((GtkCListRow*)ptr2)->data;
 
   if (a > b) {
-    return 1;
+    return -1;
   } else if (a == b) {
     return 0;
   } else {
-    return -1;
+    return 1;
   }
 
   return 0;
