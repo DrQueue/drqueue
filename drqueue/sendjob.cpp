4e0a3e804cc40117121ff68fb01c32dbdf4c3b3e
oliverhotz <oliverhotz@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2005-05-09 15:52:25 +0000

    added support for all the other renders to be invoked from the command line
    git-svn-id: https://ssl.drqueue.org/svn/trunk@1277 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- drqueue/sendjob.cpp
+++ drqueue/sendjob.cpp
@@ -90,6 +90,60 @@ int main (int argc,char *argv[])
     		exit (1);
   		}
 			break;
+		case TOJ_BMRT:
+  		if (RegisterBmrtJobFromFile (infile)) {
+				std::cerr << "Error registering BMRT job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_THREEDELIGHT:
+  		if (RegisterThreedelightJobFromFile (infile)) {
+				std::cerr << "Error registering 3Delight job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_PIXIE:
+  		if (RegisterPixieJobFromFile (infile)) {
+				std::cerr << "Error registering PIXIE job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_LIGHTWAVE:
+  		if (RegisterLightwaveJobFromFile (infile)) {
+				std::cerr << "Error registering Lightwave job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+					case TOJ_AFTEREFFECTS:
+  		if (RegisterAftereffectsJobFromFile (infile)) {
+				std::cerr << "Error registering After Effects job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_SHAKE:
+  		if (RegisterShakeJobFromFile (infile)) {
+				std::cerr << "Error registering Shake job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_AQSIS:
+  		if (RegisterAqsisJobFromFile (infile)) {
+				std::cerr << "Error registering Aqsis job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_TERRAGEN:
+  		if (RegisterTerragenJobFromFile (infile)) {
+				std::cerr << "Error registering Terragen job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
+		case TOJ_NUKE:
+  		if (RegisterNukeJobFromFile (infile)) {
+				std::cerr << "Error registering Nuke job from file: " << argv[argc-1] << std::endl;
+    		exit (1);
+  		}
+			break;
 	}
 
   std::cerr << "Job sent successfuly to the queue\n";
@@ -113,7 +167,7 @@ void usage (void)
 						<< "Valid options:\n"
 						<< "\t-v version information\n"
 						<< "\t-h prints this help\n"
-						<< "\t-t [maya|blender|mentalray] type of job\n";
+						<< "\t-t [maya|blender|mentalray|bmrt|aqsis|3delight|pixie|lightwave|terragen|nuke|aftereffects|shake] type of job\n";
 }
 
 int RegisterMayaJobFromFile (std::ifstream &infile)
@@ -324,6 +378,482 @@ int RegisterBlenderJobFromFile (std::ifstream &infile)
   return 0;
 }
 
+int RegisterLightwaveJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct lightwavesgi lightwaveSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  std::string configDir;
+	std::string projectDir;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+  getline(infile,configDir);
+	getline(infile,projectDir);
+
+//  strncpy(lightwaveSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(lightwaveSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy(lightwaveSgi.configdir,configDir.c_str(),BUFFERLEN-1);
+	strncpy(lightwaveSgi.projectdir,projectDir.c_str(),BUFFERLEN-1);
+  snprintf(lightwaveSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = lightwavesg_create(&lightwaveSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_LIGHTWAVE;
+  strncpy (job.koji.lightwave.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.lightwave.configdir,configDir.c_str(),BUFFERLEN-1);
+	strncpy (job.koji.lightwave.projectdir,projectDir.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.lightwave.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterTerragenJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct terragensgi terragenSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  std::string worldfile;
+	std::string terrainfile;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+  getline(infile,worldfile);
+	getline(infile,terrainfile);
+
+  strncpy(terragenSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(terragenSgi.scriptfile,scenePath.c_str(),BUFFERLEN-1);
+  strncpy(terragenSgi.worldfile,worldfile.c_str(),BUFFERLEN-1);
+	strncpy(terragenSgi.terrainfile,terrainfile.c_str(),BUFFERLEN-1);
+  snprintf(terragenSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = terragensg_create(&terragenSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_TERRAGEN;
+  strncpy (job.koji.terragen.scriptfile,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.terragen.worldfile,worldfile.c_str(),BUFFERLEN-1);
+	strncpy (job.koji.terragen.terrainfile,terrainfile.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.terragen.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterShakeJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct shakesgi shakeSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+
+//  strncpy(shakeSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(shakeSgi.script,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(shakeSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = shakesg_create(&shakeSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_SHAKE;
+  strncpy (job.koji.shake.script,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.shake.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterNukeJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct nukesgi nukeSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+
+//  strncpy(nukeSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(nukeSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(nukeSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = nukesg_create(&nukeSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_NUKE;
+  strncpy (job.koji.nuke.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.nuke.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterThreedelightJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct threedelightsgi threedelightSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+
+  strncpy(threedelightSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(threedelightSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(threedelightSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = threedelightsg_create(&threedelightSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_3DELIGHT;
+  strncpy (job.koji.threedelight.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.threedelight.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterPixieJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct pixiesgi pixieSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+
+//  strncpy(pixieSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(pixieSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(pixieSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = pixiesg_create(&pixieSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_PIXIE;
+  strncpy (job.koji.pixie.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.pixie.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterAftereffectsJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct aftereffectssgi aftereffectsSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  std::string comp;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+  getline(infile,comp);
+
+//  strncpy(aftereffectsSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(aftereffectsSgi.project,scenePath.c_str(),BUFFERLEN-1);
+  strncpy(aftereffectsSgi.comp,comp.c_str(),BUFFERLEN-1);
+  snprintf(aftereffectsSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = aftereffectssg_create(&aftereffectsSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_AFTEREFFECTS;
+  strncpy (job.koji.aftereffects.project,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.aftereffects.comp,comp.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.aftereffects.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int RegisterAqsisJobFromFile (std::ifstream &infile)
+{
+
+    std::cerr << "Not supported yet\n";
+    return 0;
+ 
+}
+
+int RegisterBmrtJobFromFile (std::ifstream &infile)
+{
+  // Job variables for the script generator
+  struct job job;
+  struct bmrtsgi bmrtSgi;
+
+  std::string owner;
+  std::string jobName;
+  int frameStart,frameEnd,frameStep;
+  std::string scenePath;
+  char *pathToScript;
+
+  getline(infile,owner);
+  getline(infile,jobName);
+  infile >> frameStart;
+  infile >> frameEnd;
+  infile >> frameStep;
+  getline(infile,scenePath);	//
+  getline(infile,scenePath);	// Get two times because '>>' leaves the pointer before \n 
+
+//  strncpy(bmrtSgi.file_owner,owner.c_str(),BUFFERLEN-1);
+  strncpy(bmrtSgi.scene,scenePath.c_str(),BUFFERLEN-1);
+  snprintf(bmrtSgi.scriptdir,BUFFERLEN,"%s/tmp/",getenv("DRQUEUE_ROOT"));
+
+  if (!(pathToScript = bmrtsg_create(&bmrtSgi))) {
+    std::cerr << "Error creating script file\n";
+    return 1;
+  }
+
+  strncpy (job.name,jobName.c_str(),MAXNAMELEN-1);
+  strncpy (job.cmd,pathToScript,MAXCMDLEN-1);
+  strncpy (job.owner,owner.c_str(),MAXNAMELEN-1);
+  strncpy (job.email,owner.c_str(),MAXNAMELEN-1);
+  job.frame_start = frameStart;
+  job.frame_end = frameEnd;
+  job.frame_step = frameStep;
+  job.priority = 500;
+
+  job.koj = KOJ_BMRT;
+  strncpy (job.koji.bmrt.scene,scenePath.c_str(),BUFFERLEN-1);
+  strncpy (job.koji.bmrt.viewcmd,"",BUFFERLEN-1);
+
+  job.limits.os_flags = OSF_LINUX;
+  job.limits.nmaxcpus = (uint16_t)-1;
+  job.limits.nmaxcpuscomputer = (uint16_t)-1;
+	job.limits.memory = 0;
+	strncpy (job.limits.pool,"Default",MAXNAMELEN-1);
+
+  if (!register_job(&job)) {
+    std::cerr << "Error sending job to the queue\n";
+    return 1;
+  }
+
+  return 0;
+}
+
 int str2toj (char *str)
 {
 	int toj = TOJ_NONE;
@@ -334,6 +864,24 @@ int str2toj (char *str)
 		toj = TOJ_MENTALRAY;
 	} else if (strstr(str,"blender") != NULL) {
 		toj = TOJ_BLENDER;
+	} else if (strstr(str,"bmrt") != NULL) {
+		toj = TOJ_BMRT;
+	} else if (strstr(str,"3delight") != NULL) {
+		toj = TOJ_THREEDELIGHT;
+	} else if (strstr(str,"aqsis") != NULL) {
+		toj = TOJ_AQSIS;
+	} else if (strstr(str,"lightwave") != NULL) {
+		toj = TOJ_LIGHTWAVE;
+	} else if (strstr(str,"pixie") != NULL) {
+		toj = TOJ_PIXIE;
+	} else if (strstr(str,"aftereffects") != NULL) {
+		toj = TOJ_AFTEREFFECTS;
+	} else if (strstr(str,"shake") != NULL) {
+		toj = TOJ_SHAKE;
+	} else if (strstr(str,"nuke") != NULL) {
+		toj = TOJ_NUKE;
+	} else if (strstr(str,"terragen") != NULL) {
+		toj = TOJ_TERRAGEN;		
 	}
 
 	return toj;
