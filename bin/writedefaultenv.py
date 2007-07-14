429587327b9d3aa5815a557faf13355f97b1843f
Jorge Daza Garcia-Blanes <jorge@drqueue.org>
2007-07-14 09:45:49 +0000

    Some tweaks to the new helper

--- bin/writedefaultenv.py
+++ bin/writedefaultenv.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-"""This tool will try to set a minimun set of default environment variables to have at least a local running master/slave"""
+"""This tool will try to set a minimun set of default environment variables to
+have at least a local running master/slave and will write them in bash format
+either to file received as first argument or to the one defined as
+'output_file' in case of missing that argument"""
 
 import platform
 import os
 from ConfigParser import SafeConfigParser
 import re,sys
 
+output_file='default_env'
 default_location='/usr/local'
 default_name='drqueue'
 default_display=':0'
@@ -21,18 +25,27 @@ def check_if_installed_on(location):
         print "ERROR: Installation path not found"
     return result
 
-def write_environment_file():
+def write_environment_file(output_file=output_file):
     try:
-        f=open("default_env","w+")
+        f=open(output_file,"w+")
     except:
         print "ERROR: could not create file"
-    
     f.write("export DRQUEUE_ROOT=%s%s"%(final_path,os.linesep))
-    f.write("export DISPLAY=%s%s"%(final_display,os.linesep))
+    f.write("export DRQUEUE_MASTER=%s%s"%(final_master,os.linesep))
+    if final_display:
+        f.write("export DISPLAY=%s%s"%(final_display,os.linesep))
     f.write("export PATH=$PATH:%s%s"%(final_path,os.linesep))
     f.close()
 
+# If we receive an argument we set it as the default output file
+try:
+    if sys.argv[1]:
+        output_file=sys.arg[1]
+except:
+    pass
 
+# DRQUEUE_ROOT
+final_path=default_location
 try:
     full_path = os.environ['DRQUEUE_ROOT']
 except:
@@ -64,13 +77,37 @@ except:
 else:
     print "DRQUEUE_ROOT already set to: %s"%(os.environ['DRQUEUE_ROOT'])
 
-final_display=default_display
+
+# DISPLAY
+final_display=None
 try:
-    final_display=os.environ['DISPLAY']
+    os.environ['DISPLAY']
 except:
     print "DISPLAY variable not set, setting to default: %s"
+    final_display=default_disply
 else:
     print "DISPLAY already set to: %s"%(os.environ['DISPLAY'])
 
-write_environment_file()
+# DRQUEUE_MASTER
+final_master=default_master
+try:
+    final_master=os.environ['DRQUEUE_MASTER']
+    print "DRQUEUE_MASTER was already set to '%s'. Keeping."%(final_master)
+except:
+    print "DRQUEUE_MASTER will be set to: %s"%(final_master)
+    
+write_environment_file(output_file)
 
+print """******
+After running this program successfully a file '%s' should have
+been generated with a get of default guessed environment variables.
+Please check it, and after any needed correction the way of using it
+would be to source it either directly from the command line or from
+.bash_profile or .bashrc to avoid repetition.
+
+That'd be:
+$ source %s
+
+Or:
+$ . %s
+******"""%(output_file,output_file,output_file)
