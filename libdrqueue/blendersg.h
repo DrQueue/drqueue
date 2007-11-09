43ad14c518e394c238411e3c03087e99c3ecb8ee
jorge <jorge@94d6b6b3-0ee6-0310-ab95-9b4161196ee2>
2007-11-09 20:39:13 +0000

    r2626@hiro (orig r2625):  kaazoo | 2007-10-30 12:39:04 +0100  code cleanup
    git-svn-id: https://ssl.drqueue.org/svn/branches/0.64.x@2689 94d6b6b3-0ee6-0310-ab95-9b4161196ee2

--- libdrqueue/blendersg.h
+++ libdrqueue/blendersg.h
@@ -1,5 +1,6 @@
 //
 // Copyright (C) 2001,2002,2003,2004 Jorge Daza Garcia-Blanes
+// Copyright (C) 2007 Andreas Schroeder
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
