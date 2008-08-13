05bf7c2bfd716007a622b537249c1530706b3986
Andreas Schröder <andreas@drqueue.org>
2008-08-13 21:36:00 +0000

    testing another method of diskfree

--- DrQueueOnRails/app/controllers/jobs_controller.rb
+++ DrQueueOnRails/app/controllers/jobs_controller.rb
@@ -130,21 +130,17 @@ ENV['WEB_PROTO']+"://")
     df_output = `df -m #{ ENV['DRQUEUE_TMP'] }`.split("\n")
     # check if second char of mountpoint is a "/" (47), a network mountpoint
     df_free = 0
-    
     second_char = df_output[1].split[0][1].to_i
+    
     if second_char == 47
      df_free = df_output[2].split[2].to_i
     else
-    #if second_char != 47
      df_free = df_output[1].split[3].to_i
     end
     
-    puts df_free
-    
     if df_free < 500
-    #if `df -m #{ ENV['DRQUEUE_TMP'] }`.split("\n")[1].split[1].to_i < 500
-    	flash[:notice] = 'There is less than 500 MB of free disk space avaiable. No new jobs at this time. Please contact the system administrator.' + df_free.to_s
-	   	redirect_to :action => 'list' and return
+     flash[:notice] = 'There is less than 500 MB of free disk space avaiable. No new jobs at this time. Please contact the system administrator.' + df_free.to_s
+	 redirect_to :action => 'list' and return
 	end
 	
 	
