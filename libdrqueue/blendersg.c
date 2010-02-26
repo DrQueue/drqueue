1fb71ffbb95d5cf9b8b37d784543136d23e4cab7
Andreas Schröder <andreas@drqueue.org>
2010-02-26 22:49:37 +0000

    use Jorge's jobscript API for Blender

--- libdrqueue/blendersg.c
+++ libdrqueue/blendersg.c
@@ -35,13 +35,8 @@ char *blendersg_create (struct blendersgi *info) {
   /* This function creates the blender render script based on the information given */
   /* Returns a pointer to a string containing the path of the just created file */
   /* Returns NULL on failure and sets drerrno */
-  FILE *f;
-  FILE *etc_blender_sg;   /* The blender script generator configuration file */
-  int fd_etc_blender_sg,fd_f;
+
   static char filename[BUFFERLEN];
-  char fn_etc_blender_sg[BUFFERLEN]; /* File name pointing to DRQUEUE_ETC/blender.sg */
-  char buf[BUFFERLEN];
-  int size;
   char *p;   /* Scene filename without path */
   char scene[MAXCMDLEN];
 
@@ -61,67 +56,26 @@ char *blendersg_create (struct blendersgi *info) {
   p = ( p ) ? p+1 : scene;
   snprintf(filename,BUFFERLEN-1,"%s/%s.%lX",info->scriptdir,p,(unsigned long int)time(NULL));
 
-  if ((f = fopen (filename, "a")) == NULL) {
-    if (errno == ENOENT) {
-      /* If its because the directory does not exist we try creating it first */
-      if (mkdir (info->scriptdir,0775) == -1) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      } else if ((f = fopen (filename, "a")) == NULL) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      }
-    } else {
-      drerrno = DRE_COULDNOTCREATE;
-      return NULL;
-    }
-  }
+  // TODO: Unified path handling
+  struct jobscript_info *ji = jobscript_new (JOBSCRIPT_PYTHON,filename);
 
-  /* set permissions */
-  fchmod (fileno(f),0777);
+  jobscript_write_heading (ji);
+  jobscript_set_variable (ji,"SCENE",info->scene);
 
-  /* So now we have the file open and so we must write to it */
-  fprintf(f,"#!/usr/bin/env python\n\n");
-  fprintf(f,"SCENE=\"%s\"\n",info->scene);
- 
   /* 2 means we want to distribute one single image */
   if (info->render_type == 2) {
-  	fprintf(f,"RENDER_TYPE=\"single image\"\n");
+	jobscript_set_variable (ji, "RENDER_TYPE", "single image");
   /* 1 means we want to render an animation */
   } else if (info->render_type == 1) {
-  	fprintf(f,"RENDER_TYPE=\"animation\"\n");
+	jobscript_set_variable (ji, "RENDER_TYPE", "animation");
   /* information missing */
   } else {
-  	drerrno = DRE_NOTCOMPLETE;
+        drerrno = DRE_NOTCOMPLETE;
     return NULL;
   }
 
-  /* save file so far */
-  fflush (f);
-  
-  /* script generator template */
-  snprintf(fn_etc_blender_sg,BUFFERLEN-1,"%s/blender_sg.py",getenv("DRQUEUE_ETC"));
-  
-  /* fill render script with some default values if template cannot be found */
-  if ((etc_blender_sg = fopen (fn_etc_blender_sg,"r")) == NULL) {
-    fprintf(f,"\nprint \"-------------------------------------------------\"\n");
-    fprintf(f,"print \"ATTENTION ! There was a problem opening: %s\"\n",fn_etc_blender_sg);
-    fprintf(f,"print \"So the default configuration will be used\"\n");
-    fprintf(f,"print \"-------------------------------------------------\"\n");
-    fprintf(f,"\n\n");
-    fprintf(f,"import os\n");
-    fprintf(f,"os.system(\"blender -b SCENE -f FRAME\")\n\n");
-  /* append template to script file */
-  } else {
-    fd_etc_blender_sg = fileno (etc_blender_sg);
-    fd_f = fileno (f);
-    while ((size = read (fd_etc_blender_sg,buf,BUFFERLEN)) != 0) {
-      write (fd_f,buf,size);
-    }
-    fclose(etc_blender_sg);
-  }
-
-  fclose(f);
+  jobscript_template_write (ji,"blender_sg.py");
+  jobscript_close (ji);
 
   return filename;
 }
