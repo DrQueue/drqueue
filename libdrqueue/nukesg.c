1ac9c579a8fdd32af6813d154d425bbb0516a758
kaazoo <kaazoo@drqueue.org>
2010-04-21 15:18:30 +0200

    use Jorge's jobscript API for Nuke script generator, minor fixes

--- libdrqueue/nukesg.c
+++ libdrqueue/nukesg.c
@@ -1,5 +1,8 @@
 //
 // Copyright (C) 2001,2002,2003,2004 Jorge Daza Garcia-Blanes
+// Copyright (C) 2010 Andreas Schroeder
+//
+// This file is part of DrQueue
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,8 +18,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 // USA
-//
-// $Id$
 //
 
 #include <stdio.h>
@@ -35,23 +36,18 @@
 void cygwin_conv_to_posix_path(const char *path, char *posix_path);
 #endif
 
-
 char *nukesg_create (struct nukesgi *info) {
-  /* This function creates the nuke render script based on the information given */
+  /* This function creates the Nuke render script based on the information given */
   /* Returns a pointer to a string containing the path of the just created file */
   /* Returns NULL on failure and sets drerrno */
-  FILE *f;
-  FILE *etc_nuke_sg;  /* The nuke script generator configuration file */
-  int fd_etc_nuke_sg,fd_f;
+
   static char filename[BUFFERLEN];
-  char fn_etc_nuke_sg[BUFFERLEN]; /* File name pointing to DRQUEUE_ETC/nuke.sg */
-  char buf[BUFFERLEN];
-  int size;
   char *p;   /* Scene filename without path */
   char scene[MAXCMDLEN];
+  char renderdir[MAXCMDLEN];
 
   /* Check the parameters */
-  if ((!strlen(info->scene))) {
+  if (!strlen(info->scene)) {
     drerrno = DRE_NOTCOMPLETE;
     return NULL;
   }
@@ -59,7 +55,6 @@ char *nukesg_create (struct nukesgi *info) {
 #ifdef __CYGWIN
   cygwin_conv_to_posix_path(info->scene, scene);
 #else
-
   strncpy(scene,info->scene,MAXCMDLEN-1);
 #endif
 
@@ -67,62 +62,28 @@ char *nukesg_create (struct nukesgi *info) {
   p = ( p ) ? p+1 : scene;
   snprintf(filename,BUFFERLEN-1,"%s/%s.%lX",info->scriptdir,p,(unsigned long int)time(NULL));
 
-  if ((f = fopen (filename, "a")) == NULL) {
-    if (errno == ENOENT) {
-      /* If its because the directory does not exist we try creating it first */
-      if (mkdir (info->scriptdir,0775) == -1) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      } else if ((f = fopen (filename, "a")) == NULL) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      }
-    } else {
-      drerrno = DRE_COULDNOTCREATE;
-      return NULL;
-    }
-  }
+  // TODO: Unified path handling
+  struct jobscript_info *ji = jobscript_new (JOBSCRIPT_PYTHON, filename);
 
-  fchmod (fileno(f),0777);
-
-  /* So now we have the file open and so we must write to it */
-  fprintf(f,"#!/bin/tcsh\n\n");
-  fprintf(f,"set DRQUEUE_SCENE=\"%s\"\n",info->scene);
-  fprintf(f,"set RF_OWNER=%s\n",info->file_owner);
+  jobscript_write_heading (ji);
+  jobscript_set_variable (ji,"SCENE",scene);
+  jobscript_set_variable (ji,"RF_OWNER",info->file_owner);
+  
   if (strlen(info->format)) {
-    fprintf(f,"set FFORMAT=%s\n",info->format);
+  	jobscript_set_variable (ji,"FFORMAT",info->format);
   }
   if (info->res_x != -1) {
-    fprintf(f,"set RESX=%i\n",info->res_x);
+    jobscript_set_variable_int (ji,"RESX",info->res_x);
   }
   if (info->res_y != -1) {
-    fprintf(f,"set RESY=%i\n",info->res_y);
+  	jobscript_set_variable_int (ji,"RESY",info->res_y);
   }
   if (strlen(info->camera)) {
-    fprintf(f,"set CAMERA=%s\n",info->camera);
+  	jobscript_set_variable (ji,"CAMERA",info->camera);
   }
-
-  snprintf(fn_etc_nuke_sg,BUFFERLEN-1,"%s/nuke.sg",getenv("DRQUEUE_ETC"));
-
-  fflush (f);
-
-  if ((etc_nuke_sg = fopen (fn_etc_nuke_sg,"r")) == NULL) {
-    fprintf(f,"\necho -------------------------------------------------\n");
-    fprintf(f,"echo ATTENTION ! There was a problem opening: %s\n",fn_etc_nuke_sg);
-    fprintf(f,"echo So the default configuration will be used\n");
-    fprintf(f,"echo -------------------------------------------------\n");
-    fprintf(f,"\n\n");
-    fprintf(f,"nuke -x $DRQUEUE_SCENE $DRQUEUE_FRAME-$DRQUEUE_FRAME\n\n");
-  } else {
-    fd_etc_nuke_sg = fileno (etc_nuke_sg);
-    fd_f = fileno (f);
-    while ((size = read (fd_etc_nuke_sg,buf,BUFFERLEN)) != 0) {
-      write (fd_f,buf,size);
-    }
-    fclose(etc_nuke_sg);
-  }
-
-  fclose(f);
+  
+  jobscript_template_write (ji,"nuke_sg.py");
+  jobscript_close (ji);
 
   return filename;
 }
