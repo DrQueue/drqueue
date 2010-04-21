3fbd9204558bad6f44dc670cff041022c40045f0
kaazoo <kaazoo@drqueue.org>
2010-04-21 15:25:10 +0200

    use Jorge's jobscript API for Pixie script generator, minor fixes

--- libdrqueue/pixiesg.c
+++ libdrqueue/pixiesg.c
@@ -1,5 +1,8 @@
 //
 // Copyright (C) 2001,2002,2003,2004 Jorge Daza Garcia-Blanes
+// Copyright (C) 2010 Andreas Schroeder
+//
+// This file is part of DrQueue
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,8 +19,6 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 // USA
 //
-// $Id$
-//
 
 #include <stdio.h>
 #include <time.h>
@@ -32,17 +33,13 @@
 #include "libdrqueue.h"
 
 char *pixiesg_create (struct pixiesgi *info) {
-  /* This function creates the pixie render script based on the information given */
+  /* This function creates the Pixie render script based on the information given */
   /* Returns a pointer to a string containing the path of the just created file */
   /* Returns NULL on failure and sets drerrno */
-  FILE *f;
-  FILE *etc_pixie_sg;   /* The pixie script generator configuration file */
-  int fd_etc_pixie_sg,fd_f;
+
   static char filename[BUFFERLEN];
-  char fn_etc_pixie_sg[BUFFERLEN]; /* File name pointing to DRQUEUE_ETC/pixie.sg */
-  char buf[BUFFERLEN];
-  int size;
   char *p;   /* Scene filename without path */
+  char scene[MAXCMDLEN];
 
   /* Check the parameters */
   if (!strlen(info->scene)) {
@@ -50,32 +47,25 @@ char *pixiesg_create (struct pixiesgi *info) {
     return NULL;
   }
 
-  p = strrchr(info->scene,'/');
-  p = ( p ) ? p+1 : info->scene;
-  snprintf(filename,BUFFERLEN-1,"%s/%s.%lX",info->scriptdir,p,time(NULL));
+#ifdef __CYGWIN
+  cygwin_conv_to_posix_path(info->scene, scene);
+#else
+  strncpy(scene,info->scene,MAXCMDLEN-1);
+#endif
 
-  if ((f = fopen (filename, "a")) == NULL) {
-    if (errno == ENOENT) {
-      /* If its because the directory does not exist we try creating it first */
-      if (mkdir (info->scriptdir,0775) == -1) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      } else if ((f = fopen (filename, "a")) == NULL) {
-        drerrno = DRE_COULDNOTCREATE;
-        return NULL;
-      }
-    } else {
-      drerrno = DRE_COULDNOTCREATE;
-      return NULL;
-    }
-  }
+  p = strrchr(scene,'/');
+  p = ( p ) ? p+1 : scene;
+  snprintf(filename,BUFFERLEN-1,"%s/%s.%lX",info->scriptdir,p,(unsigned long int)time(NULL));
 
-  fchmod (fileno(f),0777);
+  // TODO: Unified path handling
+  struct jobscript_info *ji = jobscript_new (JOBSCRIPT_PYTHON, filename);
 
-  /* So now we have the file open and so we must write to it */
-  fprintf(f,"#!/bin/tcsh\n\n");
-  fprintf(f,"set RIBFILE=%s\n",info->scene);
+  jobscript_write_heading (ji);
+  jobscript_set_variable (ji,"RIBFILE",scene);
+  
   /*
+  old options that may be worth to include:
+  
   fprintf(f,"set CUSTOM_CROP=%u\n",info->custom_crop);
   if (info->custom_crop) {
    fprintf(f,"set CROP_XMIN=%u\n",info->xmin);
@@ -100,33 +90,12 @@ char *pixiesg_create (struct pixiesgi *info) {
    fprintf(f,"set RAYSAMPLES=%u\n",info->raysamples);
   }
   */
-
-
-
-  snprintf(fn_etc_pixie_sg,BUFFERLEN-1,"%s/pixie.sg",getenv("DRQUEUE_ETC"));
-
-  fflush (f);
-
-  if ((etc_pixie_sg = fopen (fn_etc_pixie_sg,"r")) == NULL) {
-    fprintf(f,"\necho -------------------------------------------------\n");
-    fprintf(f,"echo ATTENTION ! There was a problem opening: %s\n",fn_etc_pixie_sg);
-    fprintf(f,"echo So the default configuration will be used\n");
-    fprintf(f,"echo -------------------------------------------------\n");
-    fprintf(f,"\n\n");
-  } else {
-    fd_etc_pixie_sg = fileno (etc_pixie_sg);
-    fd_f = fileno (f);
-    while ((size = read (fd_etc_pixie_sg,buf,BUFFERLEN)) != 0) {
-      write (fd_f,buf,size);
-    }
-    fclose(etc_pixie_sg);
-  }
-
-  fclose(f);
+  
+  jobscript_template_write (ji,"pixie_sg.py");
+  jobscript_close (ji);
 
   return filename;
 }
-
 
 char *pixiesg_default_script_path (void) {
   static char buf[BUFFERLEN];
