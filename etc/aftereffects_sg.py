fa981241651bf6e1e2282f8a6678826629609673
kaazoo <kaazoo@drqueue.org>
2010-04-22 01:55:44 +0200

    added Python script template for After Effects

---
+++ etc/aftereffects_sg.py
@@ -0,0 +1,73 @@
+#
+# THIS IS A PYTHON SCRIPT FILE
+# 
+# Default configuration for Blender script generator
+# 
+# Python variables
+# PROJECT, COMP
+# 
+# shell variables
+# DRQUEUE_BIN, DRQUEUE_ETC, DRQUEUE_OS, DRQUEUE_FRAME, DRQUEUE_ENDFRAME, DRQUEUE_BLOCKSIZE
+#
+
+#
+# For platform dependend environment setting a form like this
+# can be used :
+#
+# if DRQUEUE_OS == "LINUX":
+#    # Environment for Linux
+# elsif DRQUEUE_OS == "IRIX":
+#    # Environment for Irix
+# else
+#    # Some error messages
+#
+
+import os,signal,subprocess,sys
+
+os.umask(0)
+
+# fetch DrQueue environment
+DRQUEUE_ETC = os.getenv("DRQUEUE_ETC")
+DRQUEUE_BIN = os.getenv("DRQUEUE_BIN")
+DRQUEUE_OS = os.getenv("DRQUEUE_OS")
+DRQUEUE_FRAME = os.getenv("DRQUEUE_FRAME")
+DRQUEUE_ENDFRAME = os.getenv("DRQUEUE_ENDFRAME")
+DRQUEUE_BLOCKSIZE = os.getenv("DRQUEUE_BLOCKSIZE")
+
+
+if DRQUEUE_OS == "WINDOWS":
+	BLOCK = subprocess.Popen(["expr.exe", DRQUEUE_FRAME+" + "+DRQUEUE_BLOCKSIZE+" - 1"], stdout=subprocess.PIPE).communicate()[0]
+	# should be already done in script generator
+	#SCENE = subprocess.Popen(["cygpath.exe", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
+else:
+	BLOCK = subprocess.Popen(["expr", DRQUEUE_FRAME+" + "+DRQUEUE_BLOCKSIZE+" - 1"], stdout=subprocess.PIPE).communicate()[0]
+
+if BLOCK > DRQUEUE_ENDFRAME:
+	BLOCK = DRQUEUE_ENDFRAME
+
+ENGINE_PATH="aerender"
+
+command = ENGINE_PATH+" -project "+PROJECT+" -comp "+COMP+" -OMtemplate \"Multi-Machine Sequence\" -s "+DRQUEUE_FRAME+" -e "+BLOCK
+
+print command
+sys.stdout.flush()
+
+p = subprocess.Popen(command, shell=True)
+sts = os.waitpid(p.pid, 0)
+
+# This should requeue the frame if failed
+if sts[1] != 0:
+	print "Requeueing frame..."
+	os.kill(os.getppid(), signal.SIGINT)
+else:
+	#if DRQUEUE_OS != "WINDOWS" then:
+	# The frame was rendered properly
+	# We don't know the output image name. If we knew we could set this correctly
+	# chown_block RF_OWNER RD/IMAGE DRQUEUE_FRAME BLOCK 
+
+	# change userid and groupid
+	#chown 1002:1004 $SCENE:h/*
+	print "Finished."
+#
+# Notice that the exit code of the last command is received by DrQueue
+#
