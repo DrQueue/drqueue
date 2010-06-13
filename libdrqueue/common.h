0fbe0e02eccfe4d9ca2f090c4939ac9f4fa3d86b
Andreas Schroeder <andreas@drqueue.org>
2010-06-13 19:27:24 +0200

    update version number

--- libdrqueue/common.h
+++ libdrqueue/common.h
@@ -35,7 +35,7 @@ extern "C" {
 #define VERSION_MINOR  64
 #define VERSION_PATCH  4
 #define VERSION_POST   0
-#define VERSION_PRE    3
+#define VERSION_PRE    0
 
 
 int common_environment_check (void);
