67dc1bea8b00cf22b80efe7eb351c0786691e511
Andreas Schroeder <andreas@drqueue.org>
2010-08-26 22:20:45 +0200

    fix for ticket #140 by Dale B

--- DrQueueOnRails/app/controllers/application_controller.rb
+++ DrQueueOnRails/app/controllers/application_controller.rb
@@ -2,6 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  # By default, don't log passwords.
+  # Comment out the following line to help diagnose authentication issues.
+  filter_parameter_logging "password"
+
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_DrQueueOnRails_session_id'
   
