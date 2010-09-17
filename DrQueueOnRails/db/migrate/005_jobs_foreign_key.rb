61845c979e1e8f940b3c944a437c3d89c17b3bf3
Andreas Schroeder <andreas@drqueue.org>
2010-09-18 00:09:41 +0200

    remove DrQueueOnRails directory because it now has it's own repository

--- DrQueueOnRails/db/migrate/005_jobs_foreign_key.rb
+++
@@ -1,9 +0,0 @@
-class JobsForeignKey < ActiveRecord::Migration
-  def self.up
-  	add_column "jobs", "profile_id", :integer
-  end
-
-  def self.down
-  	remove_column "jobs", "profile_id"
-  end
-end
--- DrQueueOnRails/vendor/plugins/x_send_file/init.rb
+++
@@ -1,4 +0,0 @@
-require 'x_send_file'
-
-# Add x_send_file to ActionController
-ActionController::Base.send(:include, XSendFile::Controller)
